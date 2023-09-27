3c9ea17239d0339a59d42e1f5465aa3588452804
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-09-27 21:37:11 +0200

    init

---
+++ app.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+const genders = ['M', 'F'];
+
+const female = [
+  "Anna", "Maria", "Katarzyna", "Małgorzata", "Agnieszka", "Ewa", "Joanna", "Magdalena", "Aleksandra", "Natalia",
+  "Dorota", "Iwona", "Patrycja", "Justyna", "Monika", "Karolina", "Kamila", "Barbara", "Aneta", "Kinga",
+  "Paulina", "Renata", "Martyna", "Alicja", "Izabela", "Marzena", "Ola", "Karina", "Ania", "Klaudia",
+  "Nina", "Klaudia", "Weronika", "Daria", "Dominika", "Elżbieta", "Aurelia", "Jolanta", "Ewelina", "Agata",
+  "Marta", "Nadia", "Olga", "Sylwia", "Kornelia", "Kaja", "Elena", "Ilona", "Amelia", "Rozalia"
+];
+
+const male = [
+  "Adam", "Piotr", "Krzysztof", "Tomasz", "Andrzej", "Jan", "Michał", "Marek", "Łukasz", "Rafał",
+  "Daniel", "Wojciech", "Sebastian", "Bartosz", "Artur", "Filip", "Damian", "Mariusz", "Arkadiusz", "Grzegorz",
+  "Dariusz", "Sławomir", "Tadeusz", "Mikołaj", "Dawid", "Radosław", "Jacek", "Kamil", "Kacper", "Jarek",
+  "Robert", "Tadek", "Przemysław", "Błażej", "Kuba", "Paweł", "Mateusz", "Łukasz", "Maksymilian", "Maciej",
+  "Bartłomiej", "Dominik", "Piotrek", "Hubert", "Adrian", "Mikołaj", "Józef", "Kornel", "Bogdan", "Zbigniew"
+];
+
+const firstName = [
+  "Nowak", "Kowalski", "Wiśniewski", "Wójcik", "Kowalczyk", "Kamiński", "Lewandowski", "Zieliński", "Szymański", "Woźniak",
+  "Dąbrowski", "Kozłowski", "Jankowski", "Mazur", "Wojciechowski", "Kwiatkowski", "Krawczyk", "Kaczmarek", "Piotrowski", "Grabowski",
+  "Zając", "Pawłowski", "Michalski", "Król", "Nowakowski", "Wieczorek", "Jabłoński", "Dudek", "Adamczyk", "Majewski",
+  "Witkowski", "Walczak", "Stępień", "Gajewski", "Pawlak", "Malinowski", "Rutkowski", "Michalak", "Sikora", "Olszewski",
+  "Jaworski", "Wróbel", "Nowicki", "Bednarek", "Domański", "Zalewski", "Pająk", "Górski", "Tomczak", "Lis"
+];
+
+function randChoice(arr) {
+  const elem = Math.floor(Math.random() * arr.length);
+  return arr[elem]
+}
+
+const people = [];
+
+for (let i = 0; i < 20; i++) {
+  const person = { gender: randChoice(genders) };
+  if (person.gender === 'M') {
+    person.name = randChoice(male)
+  }
+  else {
+    person.name = randChoice(female)
+  }
+  const firstNameIndex = Math.floor(Math.random() * firstName.length);
+  person.firstName = firstName[firstNameIndex];
+  person.age = Math.floor(Math.random() * (78 - 18 + 1)) + 18;
+  people.push(person);
+}
+
+const json = JSON.stringify(people)
+
+fs.writeFile('people.json', json, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
